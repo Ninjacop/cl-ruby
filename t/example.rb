783092dd3fe5282ae6ab93ab0dfc7c0c2d983cdc
Ninjacop <tlutze@cannonschool.org>
2019-01-17 23:57:34 -0500

    Added comments and deleted funcall at the end

--- t/example.rb
+++ t/example.rb
@@ -1,7 +1,9 @@
+## example.rb
+## Created by Ninjacop123
+## Thursday, January 17th, 2019
+## An example function to be tested & called in C and/or CL
+
 # Define a function called hello
 def hello
     puts 'Hello Common Lisp!'
 end
-
-# call the hello function
-hello
