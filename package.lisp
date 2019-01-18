ee5aafa0c903815a4eddf591a1f21830a7951174
Ninjacop <tlutze@cannonschool.org>
2019-01-17 23:52:39 -0500

    Organized & updated file exports

--- package.lisp
+++ package.lisp
@@ -9,22 +9,29 @@
            #:in-ruby
            #:evaluate
            #:evaluate-or-die
-           #:module
+           #:define-class
+           #:define-subclass)
+  ;; cffi-fun-defs.lisp
+  (:export #:module
            #:submodule
            #:class-with-super
-           #:define-class
            #:subclass-with-super
-           #:define-subclass
            #:class-method
            #:module-method
            #:global
+           #:define-int 
            #:const 
            #:module-const
            #:undef
            #:const-call 
            #:module-const-call 
            #:ruby-funcall 
+           #:script
            #:require-script
            #:load-script 
-           #:load-script-or-die))
+           #:load-script-or-die
+           #:last-exception)
+  ;; ruby-types.lisp
+  (:export #:var-call
+           #:defrubyvar))
 
